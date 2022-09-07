2b42f72e6fc0d2f08d693010c49eec2d4d0e4525
salazarbarrera <68821455+salazarbarrera@users.noreply.github.com>
2022-09-07 20:13:51 -0300

    Modifying logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,20 +15,20 @@ export class AppComponent {
     console.log("poto0");
 
     BarcodeScanner.hideBackground(); // make background of WebView transparent
-    console.log("poto1");
+    console.log("flag1");
   
     document.body.classList.add("qrscanner"); // add the qrscanner class to body
 
     const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
     document.body.classList.remove("qrscanner"); // remove the qrscanner from the body
 
-    console.log("poto2");
+    console.log("flag2");
 
     // if the result has content
-    console.log("poto3");
+    console.log("flag3");
 
     if (result.hasContent) {
-      console.log("poto4");
+      console.log("flag4");
       console.log(result.content); // log the raw scanned content
     }
   };
