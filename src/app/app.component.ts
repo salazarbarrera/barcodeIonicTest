1257386c0eac34dc8eca0a320e083c1d904ed606
salazarbarrera <diego.salazar.b@sabatech.cl>
2022-09-07 18:20:11 -0400

    Init 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {    this.startScan();
+  }
+
+  async startScan() {
+    console.log("poto0");
+
+    BarcodeScanner.hideBackground(); // make background of WebView transparent
+    console.log("poto1");
+  
+    document.body.classList.add("qrscanner"); // add the qrscanner class to body
+
+    const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+    document.body.classList.remove("qrscanner"); // remove the qrscanner from the body
+
+    console.log("poto2");
+
+    // if the result has content
+    console.log("poto3");
+
+    if (result.hasContent) {
+      console.log("poto4");
+      console.log(result.content); // log the raw scanned content
+    }
+  };
+
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,33 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+
+  ionViewWillEnter() {
+    this.startScan();
+  }
+
+
+  async startScan() {
+    console.log("poto0");
+
+    BarcodeScanner.hideBackground(); // make background of WebView transparent
+    console.log("poto1");
+  
+    document.body.classList.add("qrscanner"); // add the qrscanner class to body
+
+    const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+    document.body.classList.remove("qrscanner"); // remove the qrscanner from the body
+
+    console.log("poto2");
+
+    // if the result has content
+    console.log("poto3");
+
+    if (result.hasContent) {
+      console.log("poto4");
+      console.log(result.content); // log the raw scanned content
+    }
+  };
 
 }
